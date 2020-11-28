f838369c51368936c92575fb7ba4710b5b0c6a1a
Florian <florek.gaffke@gmail.com>
2020-11-28 19:41:32 +0100

    HalfTask2

---
+++ Lab_5/Task2.Designer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Drawing;
+
+namespace Lab_5
+{
+    partial class Task2
+    {
+        /// <summary> 
+        Image black_arrow;
+        Image green_arrow;
+        Image red_arrow;
+        Bitmap bmp_g_d;
+        Bitmap bmp_b_d;
+        Bitmap bmp_r_d;
+        Bitmap bmp_g_u;
+        Bitmap bmp_b_u;
+        Bitmap bmp_r_u;
+        Bitmap bmp_g_r;
+        Bitmap bmp_b_r;
+        Bitmap bmp_r_r;
+        Bitmap bmp_g_l;
+        Bitmap bmp_b_l;
+        Bitmap bmp_r_l;
+        Bitmap[] up = new Bitmap[3];
+        Bitmap[] down = new Bitmap[3];
+        Bitmap[] right = new Bitmap[3];
+        Bitmap[] left = new Bitmap[3];
+        Random rand;
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(502, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 139);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel2.Location = new System.Drawing.Point(502, 547);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 139);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel3.Location = new System.Drawing.Point(950, 253);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(174, 221);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel4.Location = new System.Drawing.Point(89, 253);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(174, 221);
+            this.panel4.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Task2";
+            this.Size = new System.Drawing.Size(1255, 795);
+            this.Load += new System.EventHandler(this.Task2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Lab_5/Task2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_5
+{
+    public partial class Task2 : UserControl
+    {
+        public Task2()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Task2_Load(object sender, EventArgs e)
+        {
+            //wczytanie srzałek
+            black_arrow = Image.FromFile("C:\\Users\\Acer\\OneDrive\\Pulpit\\Arrow.png");
+            green_arrow = Image.FromFile("C:\\Users\\Acer\\OneDrive\\Pulpit\\Green_Arrow.png");
+            red_arrow = Image.FromFile("C:\\Users\\Acer\\OneDrive\\Pulpit\\Red_Arrow.png");
+            bmp_r_d = new Bitmap(red_arrow, 70, 70);
+
+
+            //Wszystkie możliwości dla czarnej strzałki
+            bmp_b_d = new Bitmap(black_arrow, 70, 70);
+            bmp_b_u = new Bitmap(black_arrow, 70, 70);
+            bmp_b_u.RotateFlip(RotateFlipType.Rotate180FlipX);
+            bmp_b_l = new Bitmap(black_arrow, 70, 70);
+            bmp_b_l.RotateFlip(RotateFlipType.Rotate270FlipX);
+            bmp_b_r = new Bitmap(black_arrow, 70, 70);
+            bmp_b_r.RotateFlip(RotateFlipType.Rotate90FlipX);
+
+
+            //Wczytanie ekranu początkowego
+            this.panel1.BackgroundImage = bmp_b_u;
+            this.panel2.BackgroundImage = bmp_b_d;
+            this.panel3.BackgroundImage = bmp_b_r;
+            this.panel4.BackgroundImage = bmp_b_l;
+
+            //Wszystkie możliwości dla zielonej strzałki
+            bmp_g_r = new Bitmap(green_arrow, 70, 70);
+            bmp_g_l = new Bitmap(green_arrow, 70, 70);
+            bmp_g_l.RotateFlip(RotateFlipType.Rotate180FlipY);
+            bmp_g_d = new Bitmap(green_arrow, 70, 70);
+            bmp_g_d.RotateFlip(RotateFlipType.Rotate270FlipY);
+            bmp_g_u = new Bitmap(green_arrow, 70, 70);
+            bmp_g_u.RotateFlip(RotateFlipType.Rotate90FlipY);
+
+            //Wszystkie możliwości dla czerwonej strzałki
+            bmp_r_d = new Bitmap(red_arrow, 70, 70);
+            bmp_r_u = new Bitmap(red_arrow, 70, 70);
+            bmp_r_u.RotateFlip(RotateFlipType.Rotate180FlipX);
+            bmp_r_l = new Bitmap(red_arrow, 70, 70);
+            bmp_r_l.RotateFlip(RotateFlipType.Rotate270FlipX);
+            bmp_r_r = new Bitmap(red_arrow, 70, 70);
+            bmp_r_r.RotateFlip(RotateFlipType.Rotate90FlipX);
+
+            //Przypożadkowanie do tablic
+            Bitmap[] up = { bmp_b_u , bmp_b_u , bmp_r_u };
+            Bitmap[] downn = { bmp_b_d, bmp_b_d, bmp_r_d };
+            Bitmap[] right = { bmp_b_r, bmp_b_r, bmp_r_r };
+            Bitmap[] left = { bmp_b_l, bmp_b_l, bmp_r_l };
+
+            /*
+            this.panel1.BackgroundImage = bmp_g_u;
+            this.panel2.BackgroundImage = bmp_g_d;
+            this.panel3.BackgroundImage = bmp_g_r;
+            this.panel4.BackgroundImage = bmp_g_l;
+            */
+
+            this.timer1.Interval = rand.Next(3000, 7000);
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.timer1.Stop();
+            int x = rand.Next(3);
+            int y;
+            do
+            {
+                y = rand.Next(3);
+            } while (y == x);
+        }
+    }
+}
